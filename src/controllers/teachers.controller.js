ec14f9cec6bc052b4377677237bedfa54a4a7461
John Arcila Castano <jhon.380172793@ucaldas.edu.co>
2023-06-28 19:20:21 -0500

    manejo de errores

--- src/controllers/teachers.controller.js
+++ src/controllers/teachers.controller.js
@@ -3,51 +3,71 @@ import { pool } from "../db.js";
 const table = "teacher";
 
 const getTeachers = async (req, res) => {
-  const [rows] = await pool.query(`SELECT * FROM ${table}`);
-  return rows;
+  try {
+    const [rows] = await pool.query(`SELECT * FROM ${table}`);
+    return rows;
+  } catch (error) {
+    return "Internal server error"
+  }
 };
 
 const createTeacher = async (req, res) => {
-  const { cedula, fullName, phoneNumber, address, email } = req.body;
-  const [rows] = await pool.query(`INSERT INTO ${table} (cedula, fullName, phoneNumber, address, email) VALUES (?, ?, ?, ?, ?)`, [cedula, fullName, phoneNumber, address, email]);
+  try {
+    const { cedula, fullName, phoneNumber, address, email } = req.body;
+    const [rows] = await pool.query(`INSERT INTO ${table} (cedula, fullName, phoneNumber, address, email) VALUES (?, ?, ?, ?, ?)`, [cedula, fullName, phoneNumber, address, email]);
 
-  return {
-    id: rows.insertId,
-    cedula,
-    fullName,
-    phoneNumber,
-    address,
-    email
-  };
+    return {
+      id: rows.insertId,
+      cedula,
+      fullName,
+      phoneNumber,
+      address,
+      email
+    };
+  } catch (error) {
+    return "Internal server error"
+  }
 };
 
 const getTeacherById = async (req, res) => {
-  const { id } = req.params;
-  const [rows] = await pool.query(`SELECT * FROM ${table} WHERE id = ?`, [id]);
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query(`SELECT * FROM ${table} WHERE id = ?`, [id]);
 
-  if (rows.length <= 0) return 'Teacher not found'
+    if (rows.length <= 0) return 'Teacher not found'
 
-  return rows[0];
+    return rows[0];
+  } catch (error) {
+    return "Internal server error"
+  }
 };
 
 const updateTeacher = async (req, res) => {
-  const { id } = req.params;
-  const { cedula, fullName, phoneNumber, address, email } = req.body;
-  const result = await pool.query(`UPDATE ${table} SET cedula = IFNULL(?, cedula), fullName = IFNULL(?, fullName), phoneNumber = IFNULL(?, phoneNumber), address = IFNULL(?, address), email = IFNULL(?, email) WHERE id = ?`, [cedula, fullName, phoneNumber, address, email, id]);
+  try {
+    const { id } = req.params;
+    const { cedula, fullName, phoneNumber, address, email } = req.body;
+    const result = await pool.query(`UPDATE ${table} SET cedula = IFNULL(?, cedula), fullName = IFNULL(?, fullName), phoneNumber = IFNULL(?, phoneNumber), address = IFNULL(?, address), email = IFNULL(?, email) WHERE id = ?`, [cedula, fullName, phoneNumber, address, email, id]);
 
-  if (result.affectedRows <= 0) return 'Teacher not found'
+    if (result.affectedRows <= 0) return 'Teacher not found'
 
-  const [rows] = await pool.query(`SELECT * FROM ${table} WHERE id = ?`, [id]);
+    const [rows] = await pool.query(`SELECT * FROM ${table} WHERE id = ?`, [id]);
 
-  return rows;
+    return rows;
+  } catch (error) {
+    return "Internal server error"
+  }
 };
 
 const deleteTeacher = async (req, res) => {
-  const { id } = req.params;
-  const [rows] = await pool.query(`DELETE FROM ${table} WHERE id =?`, [id]);
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query(`DELETE FROM ${table} WHERE id =?`, [id]);
 
-  if (rows.affectedRows <= 0) return 'Teacher not found'
-  return 'Teacher deleted successfully';
+    if (rows.affectedRows <= 0) return 'Teacher not found'
+    return 'Teacher deleted successfully';
+  } catch (error) {
+    return "Internal server error"
+  }
 };
 
 export {
