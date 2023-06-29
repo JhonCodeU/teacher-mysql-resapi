22070a8647dd5ea7f8b02a2c06b93597edb5f027
John Arcila Castano <jhon.380172793@ucaldas.edu.co>
2023-06-28 22:39:49 -0500

    add swagger in nodejs and working

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import { routes } from "./routes/index.routes.js";
 import { pool } from "./db/db.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -9,9 +11,28 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Teacher App Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a test API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["src/routes/route/teacher.routes.js"],
+};
+
 
 app.use("/ping", async (req, res) => {
   const result = await pool.query("SELECT 1 + 1 AS result")
@@ -19,6 +40,15 @@ app.use("/ping", async (req, res) => {
 });
 
 routes(app);
+
+const specs = swaggerJsdoc(options);
+
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+
 
 app.use((req, res, next) => {
   res.status(404).json({ error: "Not found" });
--- src/routes/route/teacher.routes.js
+++ src/routes/route/teacher.routes.js
@@ -4,6 +4,139 @@ import express from 'express';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Teacher:
+ *       type: object
+ *       required:
+ *         - Id
+ *       properties:
+ *         cedula:
+ *           type: string
+ *           description: The cedula of the teacher
+ *         fullName:
+ *           type: string
+ *           description: The full name of the teacher
+ *         phoneNumber:
+ *           type: string
+ *           description: The phone number of the teacher
+ *         address:
+ *          type: string
+ *          description: The address of the teacher
+ *         email:
+ *           type: string
+ *           description: The email of the customer
+ *       example:
+ *        cedula: "123456789"
+ *        fullName: Juan Perez
+ *        phoneNumber: 123456789
+ *        address: Calle 123
+ *        email: pepe@gmail.com
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Teachers
+ *   description: The teachers managing API
+ * /api/teacher:
+ *   get:
+ *     summary: Lists all the teachers
+ *     tags: [Teachers]
+ *     responses:
+ *       200:
+ *         description: The list of the teachers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Teacher'
+ *   post:
+ *     summary: Create a new teacher
+ *     tags: [Teachers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Teacher'
+ *     responses:
+ *       200:
+ *         description: The created teacher.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Teacher'
+ *       500:
+ *         description: Some server error
+ * /api/teacher/{id}:
+ *   get:
+ *     summary: Get the teacher by id
+ *     tags: [Teachers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The teacher id
+ *     responses:
+ *       200:
+ *         description: The teacher response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Teacher'
+ *       404:
+ *         description: The teacher was not found
+ *   put:
+ *    summary: Update the teacher by the id
+ *    tags: [Teachers]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The teacher id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Teacher'
+ *    responses:
+ *      200:
+ *        description: The teacher was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Teacher'
+ *      404:
+ *        description: The teacher was not found
+ *      500:
+ *        description: Some error happened
+ *   delete:
+ *     summary: Remove the teacher by id
+ *     tags: [Teachers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The teacher id
+ *
+ *     responses:
+ *       200:
+ *         description: The teacher was deleted
+ *       404:
+ *         description: The teacher was not found
+ */
+
 router.get('/', (req, res) => {
   getTeachers(req, res).then((data) => {
 
@@ -19,11 +152,6 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   getTeacherById(req, res).then((data) => {
-
-    if (data = "Internal server error") {
-      return error(req, res, data, 500);
-    }
-
     success(req, res, data, 200);
   }).catch((err) => {
     error(req, res, err, 404);
@@ -32,24 +160,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   createTeacher(req, res).then((data) => {
-
-    if (data = "Internal server error") {
-      return error(req, res, data, 500);
-    }
-
     success(req, res, data, 201);
   }).catch((err) => {
     error(req, res, err);
   });
 });
 
-router.patch('/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   updateTeacher(req, res).then((data) => {
-
-    if (data = "Internal server error") {
-      return error(req, res, data, 500);
-    }
-
     success(req, res, data, 200);
   }).catch((err) => {
     error(req, res, err);
@@ -58,11 +176,6 @@ router.patch('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   deleteTeacher(req, res).then((data) => {
-
-    if (data = "Internal server error") {
-      return error(req, res, data, 500);
-    }
-
     success(req, res, data, 200);
   }).catch((err) => {
     error(req, res, err);
